849a0c2b065afb68f8656900225363c791705e8a
Christopher Glenn <Chrisglenn519@yahoo.com>
2019-11-24 16:05:52 -0600

    created post requests

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,18 @@ router.get('/friends',function(req,res){
     return res.json(friendsArray)
 })
 
+router.post('/friends',function(req,res){
+    let array = req.body.survey
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(parseInt(array[i]))
+    }
+
+    friendsArray.push(
+        {
+            name: req.body.name,
+            image: req.body.image,
+            survey: newArray
+        })
+})
 module.exports = router
