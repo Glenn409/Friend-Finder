08d2e73116d6815c2df0e6040fe5103c7c1993c7
Chris Glenn <chrisglenn519@yahoo.com>
2019-11-28 01:50:43 -0600

    updated starting page path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 
 
-router.get('/',function(req,res){
+router.get('',function(req,res){
     // console.log('directing to home page!')
     res.sendFile(path.join(__dirname,'../public/home.html'))
 })
