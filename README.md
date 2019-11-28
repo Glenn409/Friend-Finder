9b584fdc230598dbb195d107e126dca2805e5cec
Chris Glenn <chrisglenn519@yahoo.com>
2019-11-28 00:43:54 -0600

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Friend-Finder
+A express program to allow a user to create a survey and returns a match with similar answers.
+
+Also created a api that a user can see and use.
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 let friendsArray = [
     {
-        name:'bob',
+        name:'Bob Stine',
         image:'https://cg3.cgsociety.org/uploads/images/medium/gpepper-bald-guy-1-033129fc-70tn.jpg',
         survey: [1,3,5,2,3,4,1,1,2,5]
     },
     {
-        name:'byron',
-        image:'https://thumbor.forbes.com/thumbor/1280x868/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F42977075%2F960x0.jpg%3Ffit%3Dscale',
-        survey: [5,5,5,5,5,5,5,5,5,5]
+        name:'Terri Grapes',
+        image:'https://timedotcom.files.wordpress.com/2017/12/terry-crews-person-of-year-2017-time-magazine-2.jpg',
+        survey: [4,5,5,3,5,5,5,2,5,1]
     },
     {
-        name:'tim',
-        image:'adfhttps://thumbor.forbes.com/thumbor/1280x868/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F42977075%2F960x0.jpg%3Ffit%3Dscale',
+        name:'Mark Mainecoon',
+        image:'https://news.nd.edu/assets/113411/james_brockmole_300.jpg',
         survey: [3,3,3,3,3,3,3,3,3,3]
     },
     {
         name:'ed',
-        image:'https://thumbor.forbes.com/thumbor/1280x868/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F42977075%2F960x0.jpg%3Ffit%3Dscale',
-        survey: [1,1,1,1,1,1,1,1,1,1]
+        image:'https://bloximages.chicago2.vip.townnews.com/bozemandailychronicle.com/content/tncms/assets/v3/editorial/0/7b/07b9d51a-9036-5b76-88c7-c486bf995a2d/5d795cd0e4a96.image.jpg',
+        survey: [1,2,3,5,1,1,2,1,2,1]
     }
 ]
 
